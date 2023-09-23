c97ac4a3b6b6ee849c9154f78b61c5d40bea036f
Kunal Kumar <kunalkumar7091679623@gmail.com>
2023-09-23 18:01:56 +0530

    adding css in create Post page

--- src/Component/CreatePost.js
+++ src/Component/CreatePost.js
@@ -1,7 +1,38 @@
+import "../Styles/create-post.css"
+
 function CreatePost(){
     return (
-        <div className="post">
-            CreatePost
+        
+        <div className="create-post">
+           <h1>Create Post</h1>
+
+            <form className="form-feald">
+                <div className="form-input">
+                    <label>Title</label>
+                    <input 
+                        placeholder="Enter Your Name"
+                    />
+                </div>
+
+                <div className="form-input">
+                    <label>subTitle</label>
+                    <input 
+                         placeholder="Enter the blog title"
+                    />
+                </div>
+
+                <div className="form-input">
+                    <label>content</label>
+                    
+                    <textarea
+                        placeholder="Describe your blog here..."
+                    />
+                </div>
+
+                <button className="submit"> submit post</button>
+
+            </form>
+
         </div>
     )
 }
